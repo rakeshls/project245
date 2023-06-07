4b08e701bf0e0a364210a96506f5935e4f9b67a0
rakeshls <73830663+rakeshls@users.noreply.github.com>
2023-06-08 00:03:04 +0530

    Add files via upload

---
+++ project245.ino
@@ -0,0 +1,24 @@
+
+const byte buzzer_gpio = 18; 
+#define POTENTIOMETER_PIN  15
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(POTENTIOMETER_PIN, INPUT);
+  pinMode(buzzer_gpio, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int analogValue = analogRead(POTENTIOMETER_PIN);
+  int brightness = map(analogValue, 0, 4095, 0, 255);
+  if (brightness > 200) {
+    Serial.println("decrease the volume ");
+    digitalWrite(buzzer_gpio, HIGH);
+    delay(100);
+  }
+  digitalWrite(buzzer_gpio, LOW);
+  Serial.println(brightness);
+  delay(30);
+}
